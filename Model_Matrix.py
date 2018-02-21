426e186e6a7057483f38afc3ae643982a08f8fb1
Emimini <emeline.buoy@gmail.com>
2018-02-21 19:19:23 +0100

    Model of connection matrix add

--- Model_Matrix.py
+++ Model_Matrix.py
@@ -2,10 +2,10 @@ def kohonen_model(n):
     
     M=[[0 for i in range (n)] for j in range (n)]
     
-    V=[0,0,0,0,1]
-    nulle=[0,0,0,0,0]
+    V = [0,0,0,0,1]
+    nulle = [0,0,0,0,0]
     
-    mat_haut=[nulle]
+    mat_haut = [nulle]
     for i in range(1,4):
         mat_haut.append(V)
     mat_haut.append([0,1,1,1,0])
@@ -20,34 +20,62 @@ def kohonen_model(n):
         mat_bas.append(V)
     mat_bas.append([1,1,1,0,0])
     
-    mat_gauche=[V,nulle,V,V,[1,0,1,1,0]]
+    mat_gauche = [V,nulle,V,V,[1,0,1,1,0]]
     
-    mat_droite=[V,V,nulle,V,[1,1,0,1,0]]
+    mat_droite = [V,V,nulle,V,[1,1,0,1,0]]
     
     
     for i in range(n):
         for j in range(n):
-            if i == 0 and j == 0:
-                M[i][j]=[nulle,V,nulle,V,[0,1,0,1,0]]
-            elif i == 0 and 0 <j < n-1:
-                M[i][j]=mat_haut
-            elif i == 0 and j==n-1:
-                M[i][j]=[nulle,nulle,V,V,[0,0,1,1,0]]
-            elif 0<i<n-1 and j == 0:
-                M[i][j]=mat_gauche
-            elif 0<i<n-1 and 0<j<n-1:
-                M[i][j]=mat_centre
-            elif 0<i<n-1 and j == n-1:
-                M[i][j]=mat_droite
-            elif i == n-1 and j == 0:
-                M[i][j]=[V,V,nulle,nulle,[1,1,0,0,0]]
-            elif i == n-1 and 0 <j < n-1:
-                M[i][j]=mat_bas
-            elif i == n-1 and j == n-1:
-                M[i][j]=[V,nulle,V,nulle,[1,0,1,0,0]]
-    
+            if i == 0:
+                if j == 0:
+                    M[i][j] = [nulle,V,nulle,V,[0,1,0,1,0]]
+                elif 0 < j < n-1:
+                    M[i][j] = mat_haut 
+                elif j == n-1:
+                    M[i][j] = [nulle,nulle,V,V,[0,0,1,1,0]]
+
+            elif 0<i<n-1:
+                if j == 0:
+                    M[i][j] = mat_gauche
+                elif 0 < j < n-1:
+                    M[i][j] = mat_centre
+                elif j == n-1:
+                    M[i][j] = mat_droite
+
+            elif i == n-1:
+                if j == 0:
+                    M[i][j] = [V,V,nulle,nulle,[1,1,0,0,0]]
+                elif 0 < j < n-1:
+                    M[i][j] = mat_bas
+                elif j == n-1:
+                    M[i][j] = [V,nulle,V,nulle,[1,0,1,0,0]]
+
     return M
+
+def snake(n):
+    M=[[0 for i in range (n)] for j in range (n)]
+    nulle=[0,0,0,0,0]
+    A=[1,0,0,0,1]
+    B=[0,0,0,1,1]
     
+    for i in range(n):
+        for j in range(n):
+            if i == 0:
+                if j%2 == 0:
+                    M[i][j] = [nulle,nulle,B,[0,0,1,0,1],[0,0,1,1,0]]
+                else:
+                    M[i][j] = [nulle,B,nulle,[0,1,0,0,1],[0,1,0,1,0]]
+            elif i == n-1:
+                if j%2 == 0:
+                    M[i][j] = [[0,0,1,0,1],nulle,nulle,A,[1,0,0,1,0]]
+                else:
+                    M[i][j] = [[0,1,0,0,1],A,nulle,nulle,[1,1,0,0,0]]
+            else:
+                M[i][j] = [B,nulle,nulle,A[1,0,0,1,0]]
+            
+                
+            
     
     
     
--- SOM.py
+++ SOM.py
@@ -33,12 +33,12 @@ def distmat(n,M):
                             dist[i0+j0][i1+j1]=p
     return dist
 class Neurone:
-    def __init__(self, i, j, row,col,data,connections):
+    def __init__(self, i, j, cote,data,connections):
         self._i=i
         self._j=j
-        self._x=i/row
-        self._y=j/col
-        self._n=row*col
+        self._x=i/cote
+        self._y=j/cote
+        self._n=cote**2
         if len(data.shape)==2:
             self._weight=np.max(data)*np.random.random(data.shape[1])
         else:
@@ -128,6 +128,7 @@ class SOM:
     
     def train(self,k,nbiter,f=gauss,dist_vect=distquad,dist_win_neur=distquad):
         
+        mat_dist=distmat(self._n,self._adj)
         eps=self._eps0+(self._epsmax-self._eps0)*(nbiter - k)/nbiter
         sig=self._sig0+(self._sigmax-self._sig0)*(nbiter - k)/nbiter
         
@@ -138,14 +139,14 @@ class SOM:
         coordvect=np.random.randint(np.shape(self._data)[0])
         vector=self._data[coordvect]
         iwin,jwin=self.winner(vector)
-        self._nodes[iwin,jwin]._weight+=eps*gauss(dist_vect(self._nodes[iwin,jwin]._weight,vector),sig)*(vector-self._nodes[iwin,jwin]._weight)
-
+        self._nodes[iwin,jwin]._weight+=eps*gauss(distquad(self._nodes[iwin,jwin]._weight,vector),sig)*(vector-self._nodes[iwin,jwin]._weight)
+        
         #Les voisins du gagnant subissent aussi les effets du changement
         
         for i in range(self._n):
             for j in range(self._n):
                 if i!=iwin or j!=jwin:
-                    coeff_dist_win_ij=gauss(dist_win_neur(self._nodes[iwin,jwin],self._nodes[i,j],sig))
+                    coeff_dist_win_ij=gauss(mat_dist[indice(iwin,jwin)][indice(i,j)],sig)
                     #Coefficient permettant de déterminer le taux d'apprentissage de tous les voisins
                     self._nodes[i,j]._weight+=coeff_dist_win_ij*eps*(vector-self._nodes[i,j]._weight)
         return coordvect,iwin,jwin
